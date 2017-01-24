df784ddaaa006ec371b121b43c6693e951f0642a
jonathan42 <jonathan.fey@laposte.net>
2017-01-24 14:51:42 +0100

    toto

--- appliweb/src/test/java/appliweb/Testapp.java
+++ appliweb/src/test/java/appliweb/Testapp.java
@@ -7,10 +7,7 @@ public class Testapp {
 	@Test
 	public void testRien(){
 		//TODO
-<<<<<<< HEAD
 		System.out.println("test");
-=======
 		System.out.println("serveur");
->>>>>>> branch 'master' of https://github.com/jonathan42/myproject-parent.git
 	}
 }
